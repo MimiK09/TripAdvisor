6f40ccfc6426bc0342261f0964a61efb913f3c52
Bouclet Emeline <mimi@MacBook-Air-de-Bouclet.local>
2023-06-17 20:52:46 +0200

    northlank adresse

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		console.log("test",{ firstname, lastname, email, subject, message });
 
-		const response = await axios.post("http://localhost:3000/form", {
+		const response = await axios.post(`${process.env.northlank}:${process.env.PORT}/form`, {
 			firstname,
 			lastname,
 			email,
