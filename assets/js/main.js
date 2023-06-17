9c7c8a4ff3f7d77d199f13bac65647fd21bcbeae
Bouclet Emeline <mimi@MacBook-Air-de-Bouclet.local>
2023-06-17 17:15:58 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+// Gestion de la modale
+
+document.addEventListener("DOMContentLoaded", () => {
+	//ouverture modale
+	document.querySelector(".myBtn-modale").addEventListener("click", () => {
+		document.querySelector(".modale").classList.add("display-flex");
+	});
+	//fermeture modale
+	document.querySelector(".close-modale").addEventListener("click", () => {
+		document.querySelector(".modale").classList.remove("display-flex");
+
+	});
+});
+
+// Gestion de la form
+
+document.addEventListener("DOMContentLoaded", () => {
+	console.log("document loaded");
+	document.querySelector(".form").addEventListener("submit", async (event) => {
+		// console.log("document loaded2");
+		// ===> Empêche le comportement par défaut de la soumission d'un formulaire = rafraîchissement de la page
+		event.preventDefault();
+		// console.log("document loaded3");
+		// ===> On récupère les valeurs de chaque input
+		const firstname = document.querySelector("#form-prenom").value;
+		const lastname = document.querySelector("#form-nom").value;
+		const email = document.querySelector("#form-mail").value;
+		const subject = document.querySelector("#form-subject").value;
+		const message = document.querySelector("#form-message").value;
+
+		console.log("test",{ firstname, lastname, email, subject, message });
+
+		const response = await axios.post("http://localhost:3000/form", {
+			firstname,
+			lastname,
+			email,
+			subject,
+			message,
+		}).catch((error) => {
+            console.log("Erreur lors de la requête:", error);
+        });
+console.log(response.data);
+	});
+});
---
+++ index-back.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
+require("dotenv").config();
+
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({
+	username: process.env.mailgun_api,
+	key: process.env.mailgun_key,
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/form", async (req, res) => {
+	try {
+
+        console.log(req.body);
+
+		const { firstname, lastname, email, subject, message } = req.body;
+
+		const messageData = {
+			from: `${firstname} ${lastname} <${email}>`,
+			to: process.env.mailto,
+			subject: `${subject}`,
+			text: `${message}`,
+		};
+
+		const response = await client.messages.create(
+			process.env.mailgun_sandbox, 
+			messageData
+		);
+
+		//   Le console.log de req.body nous affiche les données qui ont été rentrées dans les inputs (dans le formulaire frontend) :
+		console.log("je suis passée, voici la réponse", response);
+		res.status(200).json({ message: "Mail envoyé" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+app.get("/", (req, res) => {
+	res.send("server is up");
+});
+
+app.listen(3000, () => {
+	console.log("server is listening 🚀 ");
+});
